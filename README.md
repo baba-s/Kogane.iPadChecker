d53d547d84c32a9ccfe49d8780082e4c0866ca88
baba-s <shotababa1206@gmail.com>
2023-02-08 19:37:34 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Kogane.iPadChecker
+# Kogane iPad Checker
+
+iPad かどうかを確認できるクラス
+
+## 使用例
+
+```cs
+Debug.Log( iPadChecker.IsIPad );
+```
---
+++ Runtime/iPadChecker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Kogane
+{
+    /// <summary>
+    /// iPad かどうかを確認できるクラス
+    /// </summary>
+    public static class iPadChecker
+    {
+        //================================================================================
+        // 変数(static)
+        //================================================================================
+        private static bool? m_isIPad;
+
+        //================================================================================
+        // 関数(static)
+        //================================================================================
+        /// <summary>
+        /// iPad なら true を返します
+        /// </summary>
+        public static bool IsIPad =>
+#if UNITY_IOS
+            m_isIPad ??= SystemInfo.deviceModel.Contains( "iPad" );
+#else
+            false;
+#endif
+    }
+}
